1ebd270dfeafdbf3720092f107dec13aa86a55b2
Ivonaldo Graça <ivonaldo.graca@gmail.com>
2024-07-14 11:43:42 +0100

    departamos excluídos

--- src/main/java/com/example/sausis/repository/DepartamentoMedicoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.sausis.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.sausis.model.DepartamentoMedico;
-
-public interface DepartamentoMedicoRepository extends JpaRepository<DepartamentoMedico,Long> {
-    
-}
